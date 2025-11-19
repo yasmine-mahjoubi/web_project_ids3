030479a0c7ba9ab7a221e0fbc42229c1dd996aa9
yasmine-mahjoubi <158762528+yasmine-mahjoubi@users.noreply.github.com>
2025-11-19 16:56:26 +0100

    Create environment.ts
    fichier de configuration

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBLHCPwEgbnwWVX5V1kQLcab2AKqXWgEXg",
+    authDomain: "web-project-ids3.firebaseapp.com",
+    projectId: "web-project-ids3",
+    storageBucket: "web-project-ids3.firebasestorage.app",
+    messagingSenderId: "374675167134",
+    appId: "1:374675167134:web:3b2707a68d4fac93d7a88f",
+    measurementId: "G-VQ5Z39H68Q"
+  }
+};
